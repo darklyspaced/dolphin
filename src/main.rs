281c1d3e322eed3abff2b668859196853b05ddd8
darklyspaced <srohanjd@gmail.com>
2024-05-17 11:54:49 +0800

    tracing for browsing & service works

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,38 @@
-use tokio::net::{TcpListener, TcpStream};
-
 use anyhow::Result;
 use local_ip_address::local_ip;
 use mdns_sd::{ServiceDaemon, ServiceEvent, ServiceInfo};
-use tracing::info;
+use tokio::net::{TcpListener, TcpStream};
+use tracing::{info, info_span, Instrument};
+use tracing_subscriber::fmt;
+
+use std::time::Duration;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let format = fmt::format().pretty();
+    tracing_subscriber::fmt().event_format(format).init();
+
     let mdns = ServiceDaemon::new().expect("failed to create daemon");
     let service_type = "_dolphin._tcp.local.";
     let receiver = mdns.browse(service_type).expect("failed to browse");
 
-    tokio::spawn(async move {
-        while let Ok(event) = receiver.recv_async().await {
-            match event {
-                ServiceEvent::ServiceResolved(info) => {
-                    println!("new service resolved at {:?}", info);
-                }
-                other_event => {
-                    println!("Received other event: {:?}", &other_event);
+    let browse_span = info_span!("browse");
+
+    tokio::spawn(
+        async move {
+            while let Ok(event) = receiver.recv_async().await {
+                match event {
+                    ServiceEvent::ServiceResolved(info) => {
+                        info!("new service resolved: {:?}", info);
+                    }
+                    other_event => {
+                        info!("received other event: {:?}", &other_event);
+                    }
                 }
             }
         }
-    });
+        .instrument(browse_span),
+    );
 
     let ip = local_ip()?.to_string();
     let port = 5200;
@@ -43,18 +53,13 @@ async fn main() -> Result<()> {
     let listener = TcpListener::bind(format!("{}:{}", ip, port)).await?;
 
     loop {
-        let (socket, _) = listener.accept().await?;
+        let (socket, peer) = listener.accept().await?;
+        info!("new request from {}", peer);
 
-        tokio::spawn(async move {
-            // Process each socket concurrently.
-            info!("new connection detected!");
-            process(socket).await
-        });
+        tokio::spawn(process(socket));
     }
 }
 
-async fn process(stream: TcpStream) {
-    loop {
-        println!("yaya");
-    }
+async fn process(_stream: TcpStream) {
+    std::thread::sleep(Duration::from_secs(100));
 }
